de6db6247e2d2c0ea8ba74b601117472783077ed
Franco Zacco <francozacco@gmial.com>
2025-03-12 14:05:06 +0100

    ch11

---
+++ ch11/ch11-p11.11.py
@@ -0,0 +1,48 @@
+import math 
+import matplotlib.pyplot as plt
+
+
+def r_fn(GM, l, dtau, r_n, r_nm1):
+    l_2 = l**2
+    right_side = (dtau**2) * (-(GM/(r_n**2)) + (l_2/(r_n**3)) - ((3 * GM * l_2)/(r_n**4)))
+    return right_side + (2 * r_n) - r_nm1
+
+
+def phi_fn(l, dtau, r_n, r_np1, phi_n):
+    return ((4 * l * dtau)/((r_np1 + r_n)**2)) + phi_n
+
+
+def main():
+    steps = 2500
+    GM = 1
+
+    r_init = 50 * GM
+    _l_circ = r_init / (math.sqrt((r_init/GM) - 3))
+    _dphidtau_circ = _l_circ / (r_init**2)
+    _tau = 2 * math.pi * ((r_init**2) / _l_circ)
+    dtau = _tau / 500
+    dphidtau_init = 0.75 * _dphidtau_circ
+    phi_init = 0.5 * dphidtau_init * dtau
+    l = (r_init**2) * (dphidtau_init)
+
+    r_list = [r_init, r_init]
+    phi_list = [0, phi_init]
+    for i in range(1, steps):
+        r_list.append(
+            r_fn(GM, l, dtau, r_list[i], r_list[i - 1])
+        )
+        phi_list.append(
+            phi_fn(l, dtau, r_list[i], r_list[i + 1], phi_list[i])
+        )
+    
+    fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+    ax.plot(phi_list, r_list)
+    ax.set_rmax(r_init + 10)
+    ax.set_rticks([r_init])
+    ax.set_rlabel_position(0)
+    ax.grid(True)
+    ax.set_title("Precession of the perihelion", va='bottom')
+    plt.show()
+
+if __name__ == "__main__":
+    main()
